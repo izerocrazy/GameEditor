e73e47291e8f9526dd6d17be73da7c7f3ff51e38
zero kuang <izerocrazy@gmail.com>
2014-09-04 23:30:25 +0800

    创建 card 对象
    所以是以卡牌游戏……？靠！

---
+++ GameEditor/GameEditor/Logic/card.h
@@ -0,0 +1,23 @@
+/********************************************************************
+	Copyright (c) 2014-2014 uloveto.com
+
+	http://www.uloveto.com
+	
+	created:	2014/09/04
+	created:	4:9:2014   16:14
+	file base:	card
+	file ext:	h
+	author:		zero
+	
+	purpose:	card obj
+*********************************************************************/
+#ifndef __card_h__
+#define __card_h__
+
+class KCard
+{
+public:
+protected:
+private:
+};
+#endif // __card_h__
